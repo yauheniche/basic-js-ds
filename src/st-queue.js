79172bde194b4b0649c3d20250533022eaade1b4
cherkasDev <cherkasyauheni.poland@gmail.com>
2021-10-14 22:38:44 +0200

    feat: add full

--- src/st-queue.js
+++ src/st-queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 module.exports = class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head
+  }
+
+  enqueue( value ) {
+    const node = new ListNode(value);
+
+    if (this.head) {
+        this.tail.next = node;
+        this.tail = node;
+    } else {
+        this.head = node;
+        this.tail = node
+    }
+
+    this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const current = this.head;
+    this.head = this.head.next;
+    this.length--;
+
+    return current.value;
   }
 
 }
--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -19,7 +19,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function removeKFromList( l, k ) {
+  if(l == null){
+    return l;
+  }
+  while(l.value == k){
+      l = l.next;
+  }
+  thisNode = l;
+  nextNode = thisNode.next;
+  while(nextNode != null){
+      if(nextNode.value == k){
+          thisNode.next = nextNode.next;
+          if(thisNode.next == null)
+              break;
+      }
+      thisNode = thisNode.next;
+      nextNode = thisNode.next;       
+  }
+  return l;
 }
